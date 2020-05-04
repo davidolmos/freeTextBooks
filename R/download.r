827d72166817fd1f9fe0649b8329bcf0eccbd1e3
davidolmos <davidolmoscorral@gmail.com>
2020-05-04 19:28:14 +0200

    Enhanced method to import reference data (data frame storing all ebook information)

--- R/download.r
+++ R/download.r
@@ -15,33 +15,34 @@
 #' @export
 
 download_ebooks <- function(mode = "all") {
-
+  library("dplyr")
   library("glue")
-  library("tidyverse")
+  library("purrr")
+  library("tidyr")
 
   # Defines download function call using safely to capture errors (e.g. HTTP 404, Not Found) and setting binary download mode
-  safe_download <- safely(~ download.file(.x , .y, mode = "wb"))
+  safe_download <- purrr::safely(~ download.file(.x , .y, mode = "wb"))
 
   # Read csv file with required information to download each book
-  list_df <- readRDS("springer_ebooks.RData")
+  data("list_df")
   attach(list_df)
 
-  # Perform pdf dowload
+  # Perform pdf download
   if (mode == "all" | mode == "pdf") {
 
     # Creates a String of the URL Addresses
     url_pdf <- list_df$url_pdf
     url_pdf <-
-    expand_grid(isbn) %>%
-    glue_data("{url_pdf}")
+    tidyr::expand_grid(isbn) %>%
+    glue::glue_data("{url_pdf}")
 
     # Creates names for the Ebook files
     names_pdf <-
-      expand_grid(isbn) %>%
-      glue_data("{title} - {author} - {edition}.pdf")
+      tidyr::expand_grid(isbn) %>%
+      glue::glue_data("{title} - {author} - {edition}.pdf")
 
     # Perform download and store ebook files
-    walk2(url_pdf, names_pdf, safe_download)
+    purrr::walk2(url_pdf, names_pdf, safe_download)
 
   }
 
@@ -51,16 +52,16 @@ download_ebooks <- function(mode = "all") {
     # Creates a String of the URL Addresses
     url_epub <- list_df$url_epub
     url_epub <-
-    expand_grid(isbn) %>%
-    glue_data("{url_epub}")
+    tidyr::expand_grid(isbn) %>%
+    glue::glue_data("{url_epub}")
 
     # Creates names for the Ebook files
     names_epub <-
-      expand_grid(isbn) %>%
-      glue_data("{title} - {author} - {edition}.epub")
+      tidyr::expand_grid(isbn) %>%
+      glue::glue_data("{title} - {author} - {edition}.epub")
 
     # Perform download and store ebook files
-    walk2(url_epub, names_epub, safe_download)
+    purrr::walk2(url_epub, names_epub, safe_download)
 
   }
 
