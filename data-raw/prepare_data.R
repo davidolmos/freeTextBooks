c6764f7b19545a389c4793b6ebd4e59ae52b9f1d
davidolmos <davidolmoscorral@gmail.com>
2020-05-04 19:28:14 +0200

    Enhanced method to import reference data (data frame storing all ebook information)

--- data-raw/prepare_data.R
+++ data-raw/prepare_data.R
@@ -1,7 +1,18 @@
+# data-raw/mydataset.R
+# Data import and processing pipeline
 # Prepare data
 # Store springer.csv in foder data/
 
-prepare_data <- function() {
-  list_df <- read.csv("data/springer.csv", header = TRUE)
-  saveRDS(list_df, file = "springer_ebooks.RData")
-}
+library(readr)
+
+list_df <- read.csv("data-raw/springer.csv", header = TRUE)
+
+# Data processing code here...
+
+# This should be the last line.
+# Note that names are unquoted.
+# I like using overwrite = T so everytime I run the script the
+# updated objects are saved, but the default is overwrite = F
+usethis::use_data(list_df, overwrite = T)
+
+# saveRDS(list_df, file = "springer_ebooks.RData")
